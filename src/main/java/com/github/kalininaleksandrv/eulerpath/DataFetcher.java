4b27564a387a30b79f28ac740fe32f7e14d9f175
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2019-12-15 17:32:23 +0300

    parse string to list of Edges

--- src/main/java/com/github/kalininaleksandrv/eulerpath/DataFetcher.java
+++ src/main/java/com/github/kalininaleksandrv/eulerpath/DataFetcher.java
@@ -1,32 +1,51 @@
 package main.java.com.github.kalininaleksandrv.eulerpath;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class DataFetcher {
 
+
     public DataFetcher() {
     }
 
-    public String dataMainRouter(String filePath){
-        return fetchDataFromFile(filePath);
+    public String dataMainRouter(String filePath) {
+        String result = fetchDataFromFile(filePath);
+        prepareListOfKnuckle(result);
+        return result;
     }
 
-    public String fetchDataFromFile(String filePath){
+    public String fetchDataFromFile(String filePath) {
 
         StringBuilder contentBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
-        {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        }
-        catch (IOException e)
-        {
+        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+            stream.forEach(contentBuilder::append);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return contentBuilder.toString();
+    }
+
+    private void prepareListOfKnuckle(String result) {
+
+        ArrayList<Edge> listofedges = new ArrayList<>();
+
+        new ArrayList<>(Arrays.asList(result.split(",")))
+                .forEach(i -> {
+                    String [] res = i.split(":");
+                    Edge myedge = new Edge(Integer.parseInt(res [0]), Integer.parseInt(res [1]));
+                    listofedges.add(myedge);
+                });
+
+        listofedges.forEach(i -> System.out.println(i.toString()));
     }
 }
